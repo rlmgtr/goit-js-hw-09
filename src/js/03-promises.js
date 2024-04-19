9f823dbcafbe044ee9d8eae4fff9ad5fef827af2
rlmgtr <rlmagtira@gmail.com>
2024-04-19 21:40:06 +0800

    js hw item 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -10,18 +10,36 @@ amount: document.querySelector('input[name="amount"]'),
 
 const { form, delay, step, amount } = refs;
 
-form.addEventListener(submit, promiseGenerator)
+form.addEventListener('submit', promiseGenerator)
 
 function promiseGenerator(event) => {
 event.preventDefault();
+let delayValue = Number(delay.value);
+
+for (let positionValue = 1; positionValue <= 5; positionValue++) {
+
+  createPromise(positionValue, delayValue)
+  .then(({position, delay}) => {
+  Notify.success(`Fulfilled promise ${position} in ${delay} ms`);
+})
+.catch(({position, delay}) => {
+Notify.failure(`Rejected promise ${position} in ${delay} ms`)
+
+});
+
+
+}
 
 }
 
 function createPromise(position, delay) {
+return new Promise((resolve, reject) => {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    resolve({position, delay});
   } else {
-    // Reject
+    reject ({position, delay});
   }
+} )
+
 }
