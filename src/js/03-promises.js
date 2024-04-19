d697ca7bde1ee01c8d0b120ba375cda934955edc
rlmgtr <rlmagtira@gmail.com>
2024-04-19 19:49:38 +0800

    js hw item 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,22 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+refs = {
+ 
+form: document.querySelector('form.form'),
+delay: document.querySelector('input[name="delay"]'),
+step: document.querySelector('input[name="step"]'),
+amount: document.querySelector('input[name="amount"]'),
+};
+
+const { form, delay, step, amount } = refs;
+
+form.addEventListener(submit, promiseGenerator)
+
+function promiseGenerator(event) => {
+event.preventDefault();
+
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
